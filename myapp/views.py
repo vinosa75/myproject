aef31d7fe181b476fca0c7b5162160f4522d6fed
vinosa <vinosa75@gmail.com>
2021-11-09 15:14:23 +0530

    1 st commit

--- myapp/views.py
+++ myapp/views.py
@@ -61,7 +61,8 @@ def entry(request):
     # response.set_cookie("","Pravin")
     # response.set_cookie("","")
     # response.set_cookie(value=1)
-    response['Set-Cookie'] = ('=; =air;rock=;power=water; Path=/;')
+    response['Set-Cookie'] = ('rain; Path=/;')
+    # response['Set-Cookie'] = ('cloud=; Path=/;')  
     return response
 
 
